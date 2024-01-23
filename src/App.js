52494e65b03187bf4b789b988adb40946e284d06
wasabiPigg <wasabiPigg@gmail.com>
2024-01-24 01:44:48 +0900

    new

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>aa</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Reactを学ぼう
         </a>
       </header>
     </div>
